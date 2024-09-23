d5040c31d1410ac3269cdc0ba624345845d37142
kripistor <liss01062004l@gmail.com>
2024-09-23 13:25:54 +0300

    add article

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -75,6 +75,13 @@ class ProductDetailPage extends StatelessWidget {
                 style: textFont,
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Артикул: ${product.article}',
+                style: textFont,
+              ),
+            ),
           ],
         ),
       ),
